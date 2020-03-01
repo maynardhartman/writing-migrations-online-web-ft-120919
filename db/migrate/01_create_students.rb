6cbf1b68623513b72999b2c8abc044218c34637a
Maynard Hartman <mjhartman@brighthouse.com>
2020-03-01 15:39:10 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,9 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[5.2]
+    def change(*args)
+        create_table(:students) do |t|
+          t.string :name,
+          
+          t.timestamps
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "grade"
+    t.datetime "birthdate"
+  end
 
 end
